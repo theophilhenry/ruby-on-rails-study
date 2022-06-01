ba9be08f5fa1cac903123e2aad5009c206cee119
theophilhenry <s160419078@student.ubaya.ac.id>
2022-06-02 00:01:14 +0700

    Getting Started Done

--- Getting Started/blog/app/controllers/comments_controller.rb
+++ Getting Started/blog/app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
@@ -14,6 +16,6 @@ class CommentsController < ApplicationController
 
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :body)
+            params.require(:comment).permit(:commenter, :body, :status)
         end
 end
